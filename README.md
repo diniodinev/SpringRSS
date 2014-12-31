0e71655e451c02f9f0b757d528ccf5d109f3696e
diniodinev <dinio.dinev@gmail.com>
2014-12-31 12:13:28 +0200

    Add logging capabilities.

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ SpringRSS
 =========
 
 RSS Project using Spring
+
+Deploy without tests:
+
+mvn package tomcat7:run -Dmaven.test.skip=true
+
+1. Get all Articles GET
+http://localhost:9090/rss/article/
+
+
--- src/main/java/com/musala/aspects/MethodsIOAspect.java
+++ src/main/java/com/musala/aspects/MethodsIOAspect.java
@@ -1,5 +1,6 @@
-package com.musala.aspects; 
- /*
+package com.musala.aspects;
+
+/*
  * Copyright 2013 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -8,23 +9,37 @@ package com.musala.aspects;
  *
  *      http://www.apache.org/licenses/LICENSE-2.0
  *
-  * Created by dinyo.dinev on 2014.
+ * Created by dinyo.dinev on 2014.
  */
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 @Aspect
+@Component
 public class MethodsIOAspect {
 
- private static final Logger logger = LoggerFactory.getLogger(MethodsIOAspect.class);
+    private static final Logger logger = LoggerFactory.getLogger(MethodsIOAspect.class);
 
- @Before("public * *(..)")
- public void loggingBeforeAllMethods( JoinPoint point){
-   logger.info("Entering in method"+ point.getTarget().getClass());
- }
+    @Before("execution(* com.musala.controller.*Controller.*(..))")
+    public void loggingBeforeAllMethods(JoinPoint point) {
+        logger.info("\n ---------------->Entering in: " + point.getStaticPart().getSignature().toString());
+    }
+
+    @After("execution(* com.musala.controller.*Controller.*(..))")
+    public void lsoggingBeforeAllMethods(JoinPoint point) {
+        if (point.getArgs().length >= 1) {
+            logger.info("\n ---------------->Arguments of method: " + point.getStaticPart().getSignature().toString());
+            for (Object obj : point.getArgs()) {
+                logger.info("\n --------> Arg=" + obj.toString());
+            }
+        }
+        logger.info("\n ---------------->Exit from: " + point.getStaticPart().getSignature().toString());
+    }
 }
